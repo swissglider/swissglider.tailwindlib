74125d708717b64273742b680dff8be1378f05dc
swissglider <npm@mailschweiz.com>
2022-05-21 13:46:42 +0200

    ghp: init

--- src/90-Story/2-pro-atoms/Header.stories.tsx
+++ src/90-Story/2-pro-atoms/Header.stories.tsx
@@ -60,7 +60,7 @@ const StandardTemplate: ComponentStory<typeof Header> = ({ headerMenuProps: { se
     return (
         <Header
             headerTitleProps={{
-                title: 'Hallo2',
+                title: 'Hallo3',
                 textColor: 'text-red-600',
             }}
             headerMenuProps={{
